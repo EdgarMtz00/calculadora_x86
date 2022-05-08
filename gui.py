77ceffc251ebe689f0275d4b0029ce6db2cb8e0f
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2022-05-07 22:05:19 -0500

    functions scaffold

--- gui.py
+++ gui.py
@@ -4,23 +4,151 @@ win = Tk()  # This is to create a basic window
 win.geometry("391x324")  # this is for the size of the window
 win.resizable(False, False)  # this is to prevent from resizing the window
 win.title("Calculator")
-second_click = False
+
+
+def conversion_action(mode):
+    global expression
+    x = int(expression)
+
+    if mode:
+        # degrees to rad
+        input_text.set(f'{x}')
+    else:
+        # rad to degrees
+        input_text.set(f'{x}')
+    expression = x
+
+
+def sign_action():
+    # change sign_action
+    global expression
+    x = int(expression)
+
+    input_text.set(f'{-x}')
+    expression = -x
+
+
+def log_action():
+    # logarithm
+    global expression
+    x = int(expression)
+
+    input_text.set(f'{x}')
+    expression = x
+
+
+def square_action(mode):
+    global expression
+    x = int(expression)
+
+    if mode:
+        # square_action
+        input_text.set(f'{x}')
+    else:
+        # sqrt
+        input_text.set(f'{x}')
+    expression = x
+
+
+def sin_action(mode):
+    global expression
+    x = int(expression)
+
+    if mode:
+        # sin_action
+        input_text.set(f'{x}')
+    else:
+        # asin
+        input_text.set(f'{x}')
+    expression = x
+
+
+def cos_action(mode):
+    global expression
+    x = int(expression)
+
+    if mode:
+        # cos_action
+        input_text.set(f'{x}')
+    else:
+        # acos
+        input_text.set(f'{x}')
+    expression = x
+
+
+def tan_action(mode):
+    global expression
+    x = int(expression)
+
+    if mode:
+        # tan_action
+        input_text.set(f'{x}')
+    else:
+        # atan
+        input_text.set(f'{x}')
+    expression = x
+
+
+def suma_action():
+    global expression
+    global operand
+    global op
+    op = print
+    operand = int(expression)
+    expression = ""
+    input_text.set("")
+
+
+def resta_action():
+    global expression
+    global operand
+    global op
+    op = print
+    operand = int(expression)
+    expression = ""
+    input_text.set("")
+
+
+def div_action():
+    global expression
+    global operand
+    global op
+    op = print
+    operand = int(expression)
+    expression = ""
+    input_text.set("")
+
+
+def mul_action():
+    global expression
+    global operand
+    global op
+    op = print
+    operand = int(expression)
+    expression = ""
+    input_text.set("")
+
 
 actions = {
-    "conversion": print,
-    "sign": print,
-    "square": print,
-    "log": print,
-    "sin": print,
-    "cos": print,
-    "tan": print,
+    "suma_action": suma_action,
+    "resta_action": resta_action,
+    "mul_action": mul_action,
+    "div_action": div_action,
+    "conversion_action": lambda: conversion_action(True),
+    "sign_action": sign_action,
+    "square_action": lambda: square_action(True),
+    "log_action": log_action,
+    "sin_action": lambda: sin_action(True),
+    "cos_action": lambda: cos_action(True),
+    "tan_action": lambda: tan_action(True),
 }
 
 sec_actions = {
-    "log": print,
-    "sin": print,
-    "cos": print,
-    "tan": print,
+    "conversion_action": lambda: conversion_action(False),
+    "square_action": lambda: square_action(False),
+    "sin_action": lambda: sin_action(False),
+    "cos_action": lambda: cos_action(False),
+    "tan_action": lambda: tan_action(False),
 }
 
 
@@ -28,24 +156,15 @@ sec_actions = {
 # Executes the default action of a pressed
 # button that matches with action dictionary members
 def action_click(item):
-    global expression
-    global second_click
-    if second_click:
-        second_click = False
-        return
-    actions.get(str(item))(item)
+    actions.get(item)()
 
 
 # 'action_dClick function:
 # Executes the secondary action of a pressed button
 # that matches with sec_action dictionary members
-def action_double_click(event):
-    global expression
-    global second_click
-    second_click = True
-    print('second click')
-    actions.get(event)(event)
-    print('second click')
+def action_double_click(item):
+    sec_actions.get(item)()
+
 
 # 'btn_click' function :
 # This Function continuously updates the
@@ -71,12 +190,16 @@ def bt_clear():
 
 def bt_equal():
     global expression
-    result = str(eval(expression))  # 'eval':This function is used to evaluates the string expression directly
-    input_text.set(result)
+    global op
+    global operand
+    op(int(expression))
+    input_text.set(str(operand))
     expression = ""
 
 
 expression = ""
+operand = 0
+op = print
 
 # 'StringVar()' :It is used to get the instance of input field
 
@@ -111,20 +234,25 @@ Button(btns_frame, text="C", fg="black", width=10, height=3, bd=0, bg="#eee", cu
        command=lambda: bt_clear()).grid(row=0, column=0, padx=1, pady=1)
 
 # Conversion between radians and degrees
-Button(btns_frame, text="rad<->deg", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
-       command=lambda: action_click("conversion")).grid(row=0, column=1, padx=1, pady=1)
-
-# Change sign
+conv_btn = Button(btns_frame, text="rad<->deg", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
+                  command=lambda: action_click("conversion_action"))
+conv_btn.bind('<Button-1>', lambda _: action_click('conversion_action'))
+conv_btn.bind('<Button-3>', lambda _: action_double_click('conversion_action'))
+conv_btn.grid(row=0, column=1, padx=1, pady=1)
+# Change sign_action
 Button(btns_frame, text="+/-", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
-       command=lambda: action_click("sign")).grid(row=0, column=2, padx=1, pady=1)
+       command=lambda: action_click("sign_action")).grid(row=0, column=2, padx=1, pady=1)
 
 # Division
 Button(btns_frame, text="/", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
-       command=lambda: btn_click("/")).grid(row=0, column=3, padx=1, pady=1)
+       command=lambda: action_click("div_action")).grid(row=0, column=3, padx=1, pady=1)
 
 # Square number
-Button(btns_frame, text="x\u00b2", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
-       command=lambda: action_click("square"), ).grid(row=0, column=4, padx=1, pady=1)
+square_btn = Button(btns_frame, text="x\u00b2", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2")
+
+square_btn.bind('<Button-1>', lambda _: action_click('square_action'))
+square_btn.bind('<Button-3>', lambda _: action_double_click('square_action'))
+square_btn.grid(row=0, column=4, padx=1, pady=1)
 
 # second row
 
@@ -142,12 +270,12 @@ Button(btns_frame, text="9", fg="black", width=10, height=3, bd=0, bg="#fff", cu
 
 # Multiplication
 Button(btns_frame, text="*", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
-       command=lambda: btn_click("*")).grid(row=1, column=3, padx=1, pady=1)
+       command=lambda: action_click("mul_action")).grid(row=1, column=3, padx=1, pady=1)
 
 # Logarithm
-log = Button(btns_frame, text="log", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2")
-log.bind('<Double-Button-1>', lambda _: action_click('log'))
-log.bind('<Button-1>', lambda _: action_double_click('log'))
+log = Button(btns_frame, text="log_action", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2")
+log.bind('<Button-1>', lambda _: action_click('log_action'))
+log.bind('<Button-3>', lambda _: action_double_click('log_action'))
 log.grid(row=1, column=4, padx=1, pady=1)
 
 # third row
@@ -165,13 +293,13 @@ Button(btns_frame, text="6", fg="black", width=10, height=3, bd=0, bg="#fff", cu
 
 # Minus
 Button(btns_frame, text="-", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
-       command=lambda: btn_click("-")).grid(row=2, column=3, padx=1, pady=1)
+       command=lambda: action_click("resta_action")).grid(row=2, column=3, padx=1, pady=1)
 
 # Sin
-sin = Button(btns_frame, text="sin", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2")
-sin.bind('<Double-Button-1>', lambda _: action_click('sin'))
-sin.bind('<Button-1>', lambda _: action_double_click('sin'))
-sin.grid(row=2, column=4, padx=1, pady=1)
+sin_btn = Button(btns_frame, text="sin_action", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2")
+sin_btn.bind('<Button-1>', lambda _: action_click('sin_action'))
+sin_btn.bind('<Button-3>', lambda _: action_double_click('sin_action'))
+sin_btn.grid(row=2, column=4, padx=1, pady=1)
 
 # fourth row
 
@@ -189,13 +317,13 @@ Button(btns_frame, text="3", fg="black", width=10, height=3, bd=0, bg="#fff", cu
 
 # Plus
 Button(btns_frame, text="+", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2",
-       command=lambda: btn_click("+")).grid(row=3, column=3, padx=1, pady=1)
+       command=lambda: action_click("suma_action")).grid(row=3, column=3, padx=1, pady=1)
 
 # Cosine
-cos = Button(btns_frame, text="cos", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2")
-cos.bind('<Double-Button-1>', lambda _: action_click('cos'))
-cos.bind('<Button-1>', lambda _: action_double_click('cos'))
-cos.grid(row=3, column=4, padx=1, pady=1)
+cos_btn = Button(btns_frame, text="cos_action", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2")
+cos_btn.bind('<Button-1>', lambda _: action_click('cos_action'))
+cos_btn.bind('<Button-3>', lambda _: action_double_click('cos_action'))
+cos_btn.grid(row=3, column=4, padx=1, pady=1)
 # fourth row
 
 # Zero
@@ -211,9 +339,9 @@ Button(btns_frame, text="=", fg="black", width=10, height=3, bd=0, bg="#eee", cu
        command=lambda: bt_equal()).grid(row=4, column=3, padx=1, pady=1)
 
 # Tangent
-tan = Button(btns_frame, text="tan", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2")
+tan_btn = Button(btns_frame, text="tan_action", fg="black", width=10, height=3, bd=0, bg="#eee", cursor="hand2")
 
-tan.bind('<Double-Button-1>', lambda _: action_click('tan'))
-tan.bind('<Button-1>', lambda _: action_double_click('tan'))
-tan.grid(row=4, column=4, padx=1, pady=1)
+tan_btn.bind('<Button-1>', lambda _: action_click('tan_action'))
+tan_btn.bind('<Button-3>', lambda _: action_double_click('tan_action'))
+tan_btn.grid(row=4, column=4, padx=1, pady=1)
 win.mainloop()
